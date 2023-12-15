f43d0156b316facf77564c6b4db80ee097fa3846
Mohammad Rafe <mrrafi5869@gmail.com>
2023-12-15 11:43:03 +0600

    ballon bg

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -60,11 +60,11 @@ const Home = () => {
             <Services />
           </section>
           {/* correction the ballon and text with responsive */}
-          <section id='section-4' className='h-full w-full snap-start lg:bg-balloon bg-balloonMob relative bg-center lg:bg-[length:100%_90%] xl:bg-[length:100%_96%] bg-no-repeat'> 
+          <section id='section-4' className='h-full w-full snap-start md:bg-balloon bg-balloonMob relative bg-center bg-[#C4D6FF] lg:bg-[length:100%_90%] xl:bg-[length:100%_96%] bg-no-repeat'> 
             <Testimonial />
           </section>
 
-          <section id='section-5' className='h-auto xs:h-full w-full snap-start -mt-8'>
+          <section id='section-5' className='h-auto xs:h-full w-full snap-start'>
             <About />
           </section>
 
